fc87d2df6d3f3c158d547486dedf4c3121e7b3f3
Ryan Heimbuch <rheimbuch@gmail.com>
2010-07-16 10:23:17 -0600

    Allow a project to have a data-repository

--- yogo/project.rb
+++ yogo/project.rb
@@ -16,8 +16,21 @@ module Yogo
     
     has n, :collections, Yogo::Project::Collection
     
+    def data_repository
+      DataMapper.repository(data_repository_name)
+    end
+    
+    private
+    
     def self.default_data_repository_name
       :project_data
     end
+    
+    def data_repository_name
+      self.class.default_data_repository_name
+    end
+    
+    
+      
   end
 end # Yogo
