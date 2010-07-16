546905993810a851a46e8875dc2e78bf9911aabb
Ryan Heimbuch <rheimbuch@gmail.com>
2010-07-16 10:24:41 -0600

    Collection creates a managed model for user-data.

--- yogo/project/collection.rb
+++ yogo/project/collection.rb
@@ -1,5 +1,6 @@
 require 'dm-types'
 require 'yogo/project'
+require 'yogo/project/collection/data/model'
 
 module Yogo
   class Project
@@ -12,6 +13,24 @@ module Yogo
       
       belongs_to  :project,     Yogo::Project
       
+      
+      def data_repository
+        project.data_repository
+      end
+      
+      def data
+        @_data_model ||= generate_model
+      end
+      
+      private
+      
+      def generate_model
+        model = DataMapper::Model.new
+        model.send(:include, Data::Model)
+        model.project_collection = self
+        
+        return model
+      end
     end # Collection
   end # Project
 end # Yogo
