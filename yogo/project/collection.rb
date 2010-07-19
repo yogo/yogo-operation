acc0ede4c6afb38fd67808bebc5c31f240b55336
Ryan Heimbuch <rheimbuch@gmail.com>
2010-07-19 16:17:33 -0600

    reworking collections

--- yogo/project/collection.rb
+++ yogo/project/collection.rb
@@ -1,4 +1,6 @@
 require 'dm-types'
+require 'uuidtools'
+
 require 'yogo/project'
 require 'yogo/project/collection/data/model'
 
@@ -7,12 +9,11 @@ module Yogo
     class Collection
       include DataMapper::Resource
       
-      property :id,         Serial
-      property :name,       String,     :required => true
-      property :description,  Text
+      property :id,               Serial
+      property :name,             String,     :required => true
+      property :description,      Text
       
-      belongs_to  :project,     Yogo::Project
-      
+      belongs_to  :project,       Yogo::Project
       
       def data_repository
         project.data_repository
@@ -22,14 +23,38 @@ module Yogo
         @_data_model ||= generate_model
       end
       
+      def data_definition
+        {
+          :properties => {}.merge!(data_properties)
+        }
+      end
+      
+      def data_definition=(definition)
+        data_properties = definition[:properties] || {}
+      end
+      
+      after(:data_definition=) do
+        update_model
+      end
+      
+      protected
+      
+      property :data_storage_name,     String, :default => UUIDTools::UUID.timestamp_create.to_s
+      property :data_properties,       Json, :default => {}.to_json
+      
       private
       
       def generate_model
         model = DataMapper::Model.new
         model.send(:include, Data::Model)
         model.project_collection = self
-        
+        model.load_definition(data_definition)
         return model
+      end
+      
+      def update_model
+        model = data
+        model.load_definition(data_definition)
       end
     end # Collection
   end # Project
