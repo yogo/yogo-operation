c10f34d3ea2253a6ceb8fa93b4d46b6a2a153e2c
Ryan Heimbuch <rheimbuch@gmail.com>
2010-08-17 12:05:34 -0600

    Modules to allow specific datamapper models to have their storage name dynamically changed

---
+++ yogo/datamapper/model/storage_context.rb
@@ -0,0 +1,13 @@
+require 'yogo/datamapper/storage'
+
+module Yogo
+  module DataMapper
+    module Model
+      module StorageContext
+        def storage_name(*args)
+          [super, Thread.current[:yogo_storage_name]].flatten.compact.join('_')
+        end
+      end # ManagedContext
+    end # Model
+  end # DataMapper
+end # Yogo
