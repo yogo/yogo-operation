3dc7b253474d4c3d2ba4c685d5a82dbc5abc590a
Ryan Heimbuch <rheimbuch@gmail.com>
2010-09-03 13:14:45 -0600

    Removing dead code

--- yogo/property_ext.rb
+++
@@ -1,7 +0,0 @@
-unless DataMapper::Property.accepted_options.include?(:label)
-  DataMapper::Property.accept_options(:label)
-end
-
-unless DataMapper::Property.accepted_options.include?(:hidden)
-  DataMapper::Property.accept_options(:hidden)
-end
