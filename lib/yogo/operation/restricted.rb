fb951a41946427a45d96b492db5893a3fb9c7513
Ryan Heimbuch <rheimbuch@gmail.com>
2010-09-16 11:08:50 -0600

    Restricted operations should not splat arguments from inner operations when composed.

--- lib/yogo/operation/restricted.rb
+++ lib/yogo/operation/restricted.rb
@@ -2,6 +2,7 @@ require 'yogo/support/extension_module'
 require 'yogo/operation/restricted/construction'
 require 'yogo/operation/restricted/call'
 require 'yogo/operation/restricted/partial'
+require 'yogo/operation/restricted/compose'
 
 module Yogo
   module Operation
@@ -11,12 +12,14 @@ module Yogo
       instance_extensions do
         extend Restricted::Call
         extend Restricted::Partial
+        extend Restricted::Compose
       end
       
       class_extensions do
         extend Restricted::Construction
         include Restricted::Call
         include Restricted::Partial
+        include Restricted::Compose
       end
     end # Restricted
   end # Operation
---
+++ lib/yogo/operation/restricted/compose.rb
@@ -0,0 +1,20 @@
+require 'yogo/support/proc/compose'
+require 'yogo/operation/restricted/expected_type'
+
+module Yogo
+  module Operation
+    module Restricted
+      module Compose
+        include ExpectedType
+        include Yogo::Support::Proc::Compose
+        
+        # Restricted operations are always expected to accept and return
+        # the SAME type of argument. Therefore don't *splat appart the result
+        # of the composed operation 'g'.
+        def compose(g)
+          lambda{|*args| self[g[*args]]}
+        end
+      end # Partial
+    end # Restricted
+  end # Operation
+end # Yogo
